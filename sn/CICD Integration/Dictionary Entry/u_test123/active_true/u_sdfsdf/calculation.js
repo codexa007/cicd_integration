fc92988f9f9f7f95d4b05b3b4cbaa98b2ba0772c
System Administrator <admin@example.com>
2019-11-27 15:03:59 -0500

    Changes by 'System Administrator' on 'CICD Integration - 1.4.1.20' -- Build #1

---
+++ sn/CICD Integration/Dictionary Entry/u_test123/active_true/u_sdfsdf/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : CICD Integration
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-11-27 20:03:13
+ * Created By  : admin
+ * Updated On  : 2019-11-27 20:03:13
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=e285eb7e4f150010a7bd9acf9310c7ca
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
